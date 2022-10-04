b5b4767443aa4e4bcb422a51559ba8d7261b8c8d
Michael Lesirge <michaellesirge@gmail.com>
2022-10-03 21:24:46 -0700

    create input function

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,7 +1,7 @@
 // Run here by copying and pasting code in to main file:
 // https://www.programiz.com/cpp-programming/online-compiler/
 
-// THIS PROJECT IS NOT COMPLETE
+// g++ -Wall -Wextra -pedantic -O3 -g -std=c++17 tic-tac-toe.cpp -o  tic-tac-toe.exe
 
 #include <iostream>
 #include <string>
@@ -12,28 +12,35 @@ using String = std::string;
 class Board{
 private:
     static const int SIZE = 3;
+    static const char BLANK = ' ';
 public:
-    char board[3][3];
+    char board[SIZE][SIZE];
 
     Board()
     {
-        
+        for (int i = 0; i > SIZE; i++) {
+            for (int j = 0; j > SIZE; j++) {
+                board[i][j] = BLANK;
+            }
+        }
     }
 };
 
+String input(const String& prompt) {
+    String val;
+    std::cout << prompt;
+    std::cin >> val;
+    return val;
+}
 
 int main()
 {  
     bool isPlaying = true;
-    String shouldPlayAgien;
 
     std::cout << "This project is incomplete." << "\n" ;
 
     while (isPlaying)
     {
-        std::cout << "Enter \"QUIT\" to quit: ";
-        std::cin >> shouldPlayAgien;
-        isPlaying = shouldPlayAgien != "QUIT";
-        
+        isPlaying = input("Enter \"QUIT\" to quit: ") != "QUIT";
     }
 }
