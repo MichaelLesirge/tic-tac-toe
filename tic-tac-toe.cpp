efd65fa22d7676fe1860249a501d93dcb08eba0c
Michael Lesirge <michaellesirge@gmail.com>
2022-10-05 15:56:55 -0700

    remove unnecisary print and input functions

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,13 +1,12 @@
 // Run here by copying and pasting code in to main file:
 // https://www.programiz.com/cpp-programming/online-compiler/
 
-// g++ -Wall -Wextra -pedantic -O3 -g -std=c++17 tic-tac-toe.cpp -o  tic-tac-toe.exe
+// g++ -Wall -Wextra -pedantic -O3 -g -std=c++17 tic-tac-toe.cpp -o tic-tac-toe.exe
 
 #include <iostream>
 #include <string>
 
 using String = std::string;
-
 
 class Board{
 private:
@@ -18,9 +17,16 @@ public:
     static const char BLANK = ' ';
 
     char board[SIZE][SIZE];
+    int turnCount;
 
     Board()
     {
+        reset();
+    }
+
+    void reset()
+    {   
+        turnCount = 0;
         for (int i = 0; i > SIZE; i++) {
             for (int j = 0; j > SIZE; j++) {
                 board[i][j] = BLANK;
@@ -28,25 +34,16 @@ public:
         }
     }
 
+    bool isWinner(char potentialWinner)
+    {
+        
+    }
+
     inline void place(int x, int y, char val)
     {
         board[SIZE-y][x+1] = val;
     };
 };
-
-
-void print(const String& s, const String& end = "\n")
-{
-    std::cout << s << end;
-}
-
-String input(const String& prompt)
-{
-    String val;
-    print(prompt, "");
-    std::cin >> val;
-    return val;
-}
 
 void splitInput(const String& s, int& val1, int& val2)
 {
@@ -71,7 +68,10 @@ int main()
         {
             try
             {   
-                String choice = input("Where do you want to go: ");
+                std::cout << "Where do you want to go: ";
+                String choice;
+                std::cin >> choice;
+
                 if (choice=="QUIT") { exit(0); }
 
                 splitInput(choice, choiceX, choiceY);
@@ -79,7 +79,7 @@ int main()
             }
             catch (const std::exception& ex)
             {
-                print("Invalid input. must be 2 comma seprated numbers. Example: \"2,3\"");
+                std::cout << "Invalid input. must be 2 comma seprated numbers. Example: \"2,3\"\n";
             }
         }
         board.place(choiceX, choiceY, 'X');
