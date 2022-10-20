9c009232da1ff548031b70f30fe9d1899b530758
Michael Lesirge <michaellesirge@gmail.com>
2022-10-20 14:04:49 -0700

    update value with .setAttribute instead .value =

--- HTML-CSS-JS/scripts/index.js
+++ HTML-CSS-JS/scripts/index.js
@@ -1,7 +1,7 @@
 import {MIN_SIZE, MAX_SIZE, DEFAULT_SIZE} from "./consts.js"
 
 document.querySelectorAll(".size-input").forEach( el => {
-    el.value = DEFAULT_SIZE
-    el.min = MIN_SIZE
-    el.max = MAX_SIZE
+    el.setAttribute("value", DEFAULT_SIZE)
+    el.setAttribute("min", MIN_SIZE)
+    el.setAttribute("max", MAX_SIZE)
 })
