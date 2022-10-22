3c0b50c38b2f4d97214b6bbfe16b681aab0dc025
Michael Lesirge <michaellesirge@gmail.com>
2022-10-21 17:36:21 -0700

    put size input horrizontal next to each other

--- HTML-CSS-JS/scripts/index.js
+++ HTML-CSS-JS/scripts/index.js
@@ -1,7 +1,15 @@
 import { MIN_SIZE, MAX_SIZE, DEFAULT_SIZE } from "./consts.js"
 
+const max_input_len = MAX_SIZE.toString().length
+
 document.querySelectorAll("input.size").forEach((el) => {
-	el.setAttribute("value", DEFAULT_SIZE)
-	el.setAttribute("min", MIN_SIZE)
-	el.setAttribute("max", MAX_SIZE)
+	el.setAttribute("value", DEFAULT_SIZE);  // must use setAttribute for value otherwise built in reset to default does not funtion
+	el.min = MIN_SIZE
+	el.max = MAX_SIZE
+	el.oninput = () => {
+		if (el.value.length > max_input_len) el.value = el.value.slice(0, max_input_len)
+		el.value = el.replace("-", "")
+	}
+	
 })
+
