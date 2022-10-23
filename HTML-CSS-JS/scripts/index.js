fee2b55d33c8a525a6c5047719d737b470d896c1
Michael Lesirge <michaellesirge@gmail.com>
2022-10-22 17:33:10 -0700

    add win by getting input default

--- HTML-CSS-JS/scripts/index.js
+++ HTML-CSS-JS/scripts/index.js
@@ -2,8 +2,11 @@ import { MIN_SIZE, MAX_SIZE, DEFAULT_SIZE } from "./consts.js"
 
 const max_input_len = MAX_SIZE.toString().length
 
-document.querySelectorAll("input.size").forEach((el) => {
-	el.setAttribute("value", DEFAULT_SIZE);  // must use setAttribute for value otherwise built in reset to default does not funtion
+const sizeInputs = document.querySelectorAll("input.size")
+const BoardSizeInputs = document.querySelectorAll("input.board-size")
+const winConstionInput = document.querySelector("input#win-condition")
+
+sizeInputs.forEach((el) => {
 	el.min = MIN_SIZE
 	el.max = MAX_SIZE
 	el.oninput = () => {
@@ -12,3 +15,15 @@ document.querySelectorAll("input.size").forEach((el) => {
 	}
 })
 
+BoardSizeInputs.forEach((el) => {
+	el.setAttribute("value", DEFAULT_SIZE);  // must use setAttribute for value otherwise built in reset to default does not funtion
+})
+
+const winConstionExplanation = document.getElementById("explanation")
+function displayCaption() {
+	winConstionExplanation.innerText = "Get " + ((winConstionInput.value == "") ? "all the way accros" : `${winConstionInput.value} in a row`) + " to win"
+}
+
+displayCaption()
+winConstionInput.oninput = displayCaption
+console.log(winConstionInput)
