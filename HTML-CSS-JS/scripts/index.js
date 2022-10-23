1ea7279fbe85698b6f10aabf89616948439dcfd8
Michael Lesirge <michaellesirge@gmail.com>
2022-10-22 20:34:00 -0700

    check for valid win condition input

--- HTML-CSS-JS/scripts/index.js
+++ HTML-CSS-JS/scripts/index.js
@@ -1,29 +1,57 @@
 import { MIN_SIZE, MAX_SIZE, DEFAULT_SIZE } from "./consts.js"
 
-const max_input_len = MAX_SIZE.toString().length
+const maxInputLen = MAX_SIZE.toString().length
 
 const sizeInputs = document.querySelectorAll("input.size")
-const BoardSizeInputs = document.querySelectorAll("input.board-size")
+const boardSizeInputs = document.querySelectorAll("input.board-size")
 const winConstionInput = document.querySelector("input#win-condition")
+const form = document.getElementById("create-game-form")
+
+let max = -1;
+function updateMax() {
+	max = -1
+	boardSizeInputs.forEach(el => { if (el.value > max) max = el.value })
+	winConstionInput.max = max
+	displayCaption()
+}
+
+function validInput(input) {
+	input.value = input.value.replace("-", "")
+	if (input.value.length > maxInputLen) input.value = input.value.slice(0, maxInputLen)
+}
 
 sizeInputs.forEach((el) => {
 	el.min = MIN_SIZE
 	el.max = MAX_SIZE
-	el.oninput = () => {
-		el.value = el.value.replace("-", "")
-		if (el.value.length > max_input_len) el.value = el.value.slice(0, max_input_len)
-	}
+
 })
 
-BoardSizeInputs.forEach((el) => {
-	el.setAttribute("value", DEFAULT_SIZE);  // must use setAttribute for value otherwise built in reset to default does not funtion
+boardSizeInputs.forEach(el => {
+	el.setAttribute("value", DEFAULT_SIZE);  // must use setAttribute for value otherwise built in reset to default does not function
+	el.oninput = () => { 
+		validInput(el)
+		updateMax()
+	}
 })
 
 const winConstionExplanation = document.getElementById("explanation")
 function displayCaption() {
-	winConstionExplanation.innerText = "Get " + ((winConstionInput.value == "") ? "all the way accros" : `${winConstionInput.value} in a row`) + " to win"
+	if (max < winConstionInput.value) {
+		winConstionExplanation.innerText = `Impossible to get ${winConstionInput.value} in a row with current board sizes.`
+	}
+	else {
+		winConstionExplanation.innerText =  "Get " + ((winConstionInput.value == "") ? "all the way accros" : `${winConstionInput.value} in a row`) + " to win."
+	}
 }
 
+winConstionInput.oninput = () => {
+	validInput(winConstionInput)
+	displayCaption()
+}
+
+updateMax()
 displayCaption()
-winConstionInput.oninput = displayCaption
-console.log(winConstionInput)
+form.onreset = () => {
+	updateMax()
+	displayCaption()
+}
