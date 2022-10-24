8b876ba53879d607d24e15f094f07fce80c00e78
Michael Lesirge <michaellesirge@gmail.com>
2022-10-23 18:11:57 -0700

    make timer consistant with main

--- HTML-CSS-JS/scripts/game.js
+++ HTML-CSS-JS/scripts/game.js
@@ -113,7 +113,7 @@ class Board {
 
 			displayInfo(players[this.currentPlayerIndex] + "s turn.")
 
-			console.time("Async win check")
+			console.time("Async-win-check")
 			let isWinner, winningArray
 
 			try {
@@ -125,7 +125,7 @@ class Board {
 					throw error 
 				}
 			}
-			console.timeEnd("Async win check")
+			console.timeEnd("Async-win-check")
 		
 
 			if (isWinner) {
