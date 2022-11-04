693402831f669bfda1d3854ff7cd128dbf6e3e0c
Michael Lesirge <michaellesirge@gmail.com>
2022-11-03 21:00:56 -0700

    get input

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -6,18 +6,21 @@
 #include <iostream>
 #include <string>
 
+#define print(x) std::cout << x
+#define println(x) std::cout << x << '\n'
+
 using String = std::string;
 
 class Board{
 private:
 public:
-    static const int SIZE = 3;
-    static const int TOTAL_SIZE = SIZE*SIZE;
+    static const uint8_t SIZE = 3;
+    static const uint16_t TOTAL_SIZE = SIZE*SIZE;
 
     static const char BLANK = ' ';
 
     char board[SIZE][SIZE];
-    int turnCount;
+    size_t turnCount;
 
     Board()
     {
@@ -42,65 +45,91 @@ public:
     inline void place(int x, int y, char val)
     {
         board[SIZE-y][x+1] = val;
-    };
+    }
 
-    const String& toString() {
-        return ""
+    const String toString() const {
+        return "";     
     }
 };
 
-std::ostream &operator<<(std::ostream &streem, Board const &board) { 
+std::ostream &operator<<(std::ostream &streem, const Board &board) { 
     streem << board.toString();
+    return streem;
 }
 
-void splitInput(const String& s, int& val1, int& val2)
+void getPosInput(const String& s, size_t& verticlePos, size_t& horizontalPos)
 {
-    int mid = s.find_first_of(' ');
-    val1 = std::stoi(s.substr(0, mid));
-    val2 = std::stoi(s.substr(mid+1, s.length()));
+    const int mid = s.find_first_of(" ");
+
+    const String stringVerticlePos = s.substr(0, mid);
+    const String stringHorizontalPos =  s.substr(mid+1, s.length());
+    if (stringVerticlePos == "top") {
+        verticlePos = 0;
+    }
+    else if (stringVerticlePos == "center") {
+        verticlePos = 1;
+    }
+    else if (stringVerticlePos == "buttom") {
+        verticlePos = 2;
+    }
+    else {
+        throw std::invalid_argument("Verticle postion (first value) must be \"top\", \"center\", or \"buttom\".");
+    }
+
+    if (stringHorizontalPos == "left") {
+        horizontalPos = 0;
+    }
+    else if (stringHorizontalPos == "center") {
+        horizontalPos = 1;
+    }
+    else if (stringHorizontalPos == "right") {
+        horizontalPos = 2;
+    }
+    else {
+        throw std::invalid_argument("Horizontal postion (second value) must be \"left\", \"center\", or \"right\".");
+    }
+
 }
-
-enum class VerticlePos {
-    top, middle, buttom
-};
-
-enum class HorizontalPos {
-    left, middle, right
-};
 
 int main()
 {   
     bool isPlaying = true;
-    VerticlePos choiceX;
-    HorizontalPos choiceY;
+    size_t choiceX;
+    size_t choiceY;
 
     Board board;
 
-    std::cout << "This project is incomplete." << "\n\n" ;
+    String choice;
+
+    println("This project is incomplete.\n");
 
     while(isPlaying)
     {   
         bool hasGotValidInput = false;
         while(!hasGotValidInput)
-        {
+        {   
+            print("Where do you want to go: ");
+            std::getline(std::cin, choice);
+
+            if (choice=="QUIT") { 
+                println("Good Bye");
+                exit(0);
+            }
             try
             {   
-                std::cout << "Where do you want to go: ";
-                String choice;
-                std::cin >> choice;
-
-                if (choice=="QUIT") { exit(0); }
-
-                splitInput(choice, choiceX, choiceY);
+                getPosInput(choice, choiceX, choiceY);
                 hasGotValidInput = true;
             }
-            catch (const std::exception& ex)
+            catch (const std::invalid_argument& ex)
             {
-                std::cout << "Invalid input. must be 2 comma seprated numbers. Example: \"2,3\"\n";
+                println(ex.what());
             }
         }
         board.place(choiceX, choiceY, 'X');
+        println(board);
+        
+        println(choiceX << ", " << choiceY);
+        println("");
 
-        std::cout << choiceX << ", " << choiceY << '\n';
     }
 }
