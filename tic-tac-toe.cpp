cea7eb5e7ebb19c17059a81c70691d27728a230f
Michael Lesirge <michaellesirge@gmail.com>
2022-11-03 22:38:56 -0700

    improve place methods

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -42,9 +42,25 @@ public:
         
     // }
 
-    inline void place(size_t x, size_t y, char val)
+    void placePlayer(size_t x, size_t y, char player)
     {
-        board[y][x] = val;
+        turnCount++;
+        place(y, x, player);
+    }
+
+    inline bool isBoardFull() const
+    {
+        return turnCount >= TOTAL_SIZE;
+    }
+
+    inline char get(size_t i, size_t j) const
+    {
+        return board[i][j];
+    }
+
+    inline void place(size_t i, size_t j, char val)
+    {
+        board[i][j] = val;
     }
 
     const String toString() const {
@@ -68,38 +84,65 @@ std::ostream &operator<<(std::ostream &streem, const Board &board) {
     return streem;
 }
 
-void getPosInput(const String& s, size_t& verticlePos, size_t& horizontalPos)
+void getPosInput(const String& s, size_t& choiceX, size_t& choiceY)
 {
     const int mid = s.find_first_of(" ");
 
-    const String stringVerticlePos = s.substr(0, mid);
-    const String stringHorizontalPos =  s.substr(mid+1, s.length());
-    if (stringVerticlePos == "top") {
-        verticlePos = 0;
+    const String verticlePos = s.substr(0, mid);
+    const String horizontalPos =  s.substr(mid+1, s.length());
+
+    // I know this is not great code but it is the simplest and it works. I tried doing stuff with enums, switch case
+    if (verticlePos == "top") {
+        choiceY = 0;
     }
-    else if (stringVerticlePos == "center") {
-        verticlePos = 1;
+    else if (verticlePos == "center" || verticlePos == "middle") {
+        choiceY = 1;
     }
-    else if (stringVerticlePos == "buttom") {
-        verticlePos = 2;
+    else if (verticlePos == "buttom") {
+        choiceY = 2;
     }
     else {
         throw std::invalid_argument("Verticle postion (first value) must be \"top\", \"center\", or \"buttom\".");
     }
 
-    if (stringHorizontalPos == "left") {
-        horizontalPos = 0;
+    if (horizontalPos == "left") {
+        choiceX = 0;
     }
-    else if (stringHorizontalPos == "center") {
-        horizontalPos = 1;
+    else if (horizontalPos == "center" || horizontalPos == "middle") {
+        choiceX = 1;
     }
-    else if (stringHorizontalPos == "right") {
-        horizontalPos = 2;
+    else if (horizontalPos == "right") {
+        choiceX = 2;
     }
     else {
         throw std::invalid_argument("Horizontal postion (second value) must be \"left\", \"center\", or \"right\".");
     }
 
+}
+
+void getValidInput(size_t& choiceX, size_t& choiceY) {
+    bool hasGotValidInput = false;
+
+    String choice;
+    while(!hasGotValidInput)
+    {   
+        print("Where do you want to go: ");
+        std::getline(std::cin, choice);
+
+        if (choice=="QUIT") { 
+            println("Good Bye");
+            exit(0);
+        }
+        try
+        {   
+            getPosInput(choice, choiceX, choiceY);
+            hasGotValidInput = true;
+        }
+        catch (const std::invalid_argument& ex)
+        {
+            println(ex.what());
+        }
+    }
 }
 
 int main()
@@ -110,37 +153,15 @@ int main()
 
     Board board;
 
-    String choice;
-
     println("This project is incomplete.\n");
 
     while(isPlaying)
     {   
-        bool hasGotValidInput = false;
-        while(!hasGotValidInput)
-        {   
-            print("Where do you want to go: ");
-            std::getline(std::cin, choice);
-
-            if (choice=="QUIT") { 
-                println("Good Bye");
-                exit(0);
-            }
-            try
-            {   
-                getPosInput(choice, choiceX, choiceY);
-                hasGotValidInput = true;
-            }
-            catch (const std::invalid_argument& ex)
-            {
-                println(ex.what());
-            }
-        }
-        board.place(choiceX, choiceY, 'X');
         println(board);
 
-        // println(choiceX << ", " << choiceY);
-        // println("");
+        getValidInput(choiceX, choiceY); // store valid location from user in choice X,Y
+
+        board.placePlayer(choiceX, choiceY, 'X'); // place current player at choice X, Y
 
     }
 }
