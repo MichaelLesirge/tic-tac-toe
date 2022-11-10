bdba139e073db8c512811276aa203641e8761a6b
Michael Lesirge <michaellesirge@gmail.com>
2022-11-09 21:45:22 -0800

    change format

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -11,11 +11,12 @@
 
 using String = std::string;
 
-class Board{
+class Board
+{
 private:
 public:
     static const size_t SIZE = 3;
-    static const size_t TOTAL_SIZE = SIZE*SIZE;
+    static const size_t TOTAL_SIZE = SIZE * SIZE;
 
     static const char FILLER = '-';
 
@@ -28,10 +29,12 @@ public:
     }
 
     void reset()
-    {   
+    {
         turnCount = 0;
-        for (size_t i = 0; i < SIZE; i++) {
-            for (size_t j = 0; j < SIZE; j++) {
+        for (size_t i = 0; i < SIZE; i++)
+        {
+            for (size_t j = 0; j < SIZE; j++)
+            {
                 board[i][j] = FILLER;
             }
         }
@@ -39,7 +42,7 @@ public:
 
     // bool isWinner(char potentialWinner) const
     // {
-        
+
     // }
 
     void placePlayer(size_t x, size_t y, char player)
@@ -63,7 +66,8 @@ public:
         board[i][j] = val;
     }
 
-    const String toString() const {
+    const String toString() const
+    {
         String out = "";
         for (size_t i = 0; i < SIZE; i++)
         {
@@ -74,71 +78,82 @@ public:
             }
             out.push_back('\n');
         }
-        
-        return out; 
+
+        return out;
     }
 };
 
-std::ostream &operator<<(std::ostream &streem, const Board &board) { 
+std::ostream &operator<<(std::ostream &streem, const Board &board)
+{
     streem << board.toString();
     return streem;
 }
 
-void getPosInput(const String& s, size_t& choiceX, size_t& choiceY)
+void getPosInput(const String &s, size_t &choiceX, size_t &choiceY)
 {
     const int mid = s.find_first_of(" ");
 
     const String verticlePos = s.substr(0, mid);
-    const String horizontalPos =  s.substr(mid+1, s.length());
+    const String horizontalPos = s.substr(mid + 1, s.length());
 
     // I know this is not great code but it is the simplest and it works. I tried doing stuff with enums, switch case
-    if (verticlePos == "top") {
+    if (verticlePos == "top")
+    {
         choiceY = 0;
     }
-    else if (verticlePos == "center" || verticlePos == "middle") {
+    else if (verticlePos == "center" || verticlePos == "middle")
+    {
         choiceY = 1;
     }
-    else if (verticlePos == "buttom") {
+    else if (verticlePos == "buttom")
+    {
         choiceY = 2;
     }
-    else {
+    else
+    {
         throw std::invalid_argument("Verticle postion (first value) must be \"top\", \"center\", or \"buttom\".");
     }
 
-    if (horizontalPos == "left") {
+    if (horizontalPos == "left")
+    {
         choiceX = 0;
     }
-    else if (horizontalPos == "center" || horizontalPos == "middle") {
+    else if (horizontalPos == "center" || horizontalPos == "middle")
+    {
         choiceX = 1;
     }
-    else if (horizontalPos == "right") {
+    else if (horizontalPos == "right")
+    {
         choiceX = 2;
     }
-    else {
+    else
+    {
         throw std::invalid_argument("Horizontal postion (second value) must be \"left\", \"center\", or \"right\".");
     }
-
 }
 
-void getValidInput(size_t& choiceX, size_t& choiceY) {
+void getValidInput(size_t &choiceX, size_t &choiceY)
+{
     bool hasGotValidInput = false;
 
     String choice;
-    while(!hasGotValidInput)
-    {   
+    while (!hasGotValidInput)
+    {
         print("Where do you want to go: ");
         std::getline(std::cin, choice);
 
-        if (choice=="QUIT") { 
+        if (choice == "QUIT")
+        {
             println("Good Bye");
             exit(0);
         }
+
         try
-        {   
+        {
             getPosInput(choice, choiceX, choiceY);
             hasGotValidInput = true;
         }
-        catch (const std::invalid_argument& ex)
+        catch (const std::invalid_argument &ex)
         {
             println(ex.what());
         }
@@ -146,7 +161,7 @@ void getValidInput(size_t& choiceX, size_t& choiceY) {
 }
 
 int main()
-{   
+{
     bool isPlaying = true;
     size_t choiceX;
     size_t choiceY;
@@ -155,13 +170,12 @@ int main()
 
     println("This project is incomplete.\n");
 
-    while(isPlaying)
-    {   
+    while (isPlaying)
+    {
         println(board);
 
         getValidInput(choiceX, choiceY); // store valid location from user in choice X,Y
 
         board.placePlayer(choiceX, choiceY, 'X'); // place current player at choice X, Y
-
     }
 }
