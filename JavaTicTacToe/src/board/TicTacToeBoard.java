975c644d20d0dcb4b7954506d75b582e3cd3d90e
Michael Lesirge <michaellesirge@gmail.com>
2022-11-19 12:24:01 -0800

    change tictactoe board constructors

--- JavaTicTacToe/src/board/TicTacToeBoard.java
+++ JavaTicTacToe/src/board/TicTacToeBoard.java
@@ -3,25 +3,40 @@ package board;
 import tictactoe.Player;
 
 public class TicTacToeBoard extends Board<Player> {
-    protected final static int DEFAULT_SIZE = 3;
+    private final static int DEFAULT_SIZE = 3;
 
-    protected final int peicesToWinHorizontal;
-    protected final int peicesToWinVertical;
+    private final int peicesToWinHorizontal;
+    private final int peicesToWinVertical;
+
+    private final boolean checkDiagnals;
 
     public TicTacToeBoard() {
         this(TicTacToeBoard.DEFAULT_SIZE, TicTacToeBoard.DEFAULT_SIZE);
     }
 
+    /**
+     * Construct a width by height board where you win by getting accros the board
+     */
     public TicTacToeBoard(int width, int height) {
-        this(width, height, width, height);
+        this(width, height, width, height, width == height);
     }
 
+    /**
+     * Construct a width by height board where you win by getting the specified
+     * number of peicesToWin in a row
+     */
     public TicTacToeBoard(int width, int height, int peicesToWin) {
-        this(width, height, peicesToWin, peicesToWin);
+        this(width, height, peicesToWin, peicesToWin, true);
     }
 
-    protected TicTacToeBoard(int width, int height, int peicesToWinHorizontal, int peicesToWinVertical) {
+    public TicTacToeBoard(
+            int width, int height,
+            int peicesToWinHorizontal, int peicesToWinVertical,
+            boolean checkDiagnals) {
+
         super(width, height);
+
+        this.checkDiagnals = checkDiagnals;
 
         this.peicesToWinHorizontal = peicesToWinHorizontal;
         this.peicesToWinVertical = peicesToWinVertical;
@@ -31,8 +46,12 @@ public class TicTacToeBoard extends Board<Player> {
         return super.isFull();
     }
 
-    // abstract public boolean isPlayerWinner(Player player); 
     public boolean isPlayerWinner(Player player) {
+        
+        if (this.checkDiagnals) {
+
+        }
+        
         return false;
-    } 
+    }
 }
