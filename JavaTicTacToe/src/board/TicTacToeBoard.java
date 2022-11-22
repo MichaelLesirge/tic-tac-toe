b85c822b38cf1a3f88938de9cf9a3eee5d448cec
Michael Lesirge <michaellesirge@gmail.com>
2022-11-21 20:37:02 -0800

    a

--- JavaTicTacToe/src/board/TicTacToeBoard.java
+++ JavaTicTacToe/src/board/TicTacToeBoard.java
@@ -47,11 +47,39 @@ public class TicTacToeBoard extends Board<Player> {
     }
 
     public boolean isPlayerWinner(Player player) {
-        
+        // check verticle 
+        for (int row = 0; row < this.height; row++) {
+            int count = 0;
+            for (int col = 0; col < this.width; col++) {
+                System.out.println(count);
+                if (this.get(row, col) == player) {
+                    count++;
+                    if (count > this.peicesToWinVertical) {
+                        return true;
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+        // check horizontal
+        for (int col = 0; col < this.width; col++) {
+            int count = 0;
+            for (int row = 0; row < this.height; row++) {
+                if (this.get(row, col) == player) {
+                    count++;
+                    if (count > this.peicesToWinHorizontal) {
+                        return true;
+                    }
+                }
+            }
+        }
+
         if (this.checkDiagnals) {
+            // check top left to buttom right
 
         }
-        
+
         return false;
     }
 }
