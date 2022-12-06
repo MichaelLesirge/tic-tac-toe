6ee4a88b3716671a682678e4da6d59f791579514
Michael Lesirge <michaellesirge@gmail.com>
2022-12-05 22:15:02 -0800

    make work for any dimentions

--- better-tic-tac-toe.py
+++ better-tic-tac-toe.py
@@ -1,12 +1,13 @@
+def create_board(*dimentions):
+    if len(dimentions) == 0:
+        return None
+    return [
+       create_board(*dimentions[:-1]) for i in range(dimentions[-1])
+    ]
+
 class Board:
-    def __init__(self, width=3, height=3, depth=1) -> None:
-        self.board = [
-            [
-                [
-                    None for k in range(width)
-                ] for j in range(height)
-            ] for i in range(depth)
-        ]
+    def __init__(self, *dimentions) -> None:
+        self.board = create_board(*dimentions)
         print(self.board)
 
-Board(3, 3, 3)
+Board(4, 3, 2)
