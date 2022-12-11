8c36926f835acbe6bbcc68532b4e5b2a5e4f480b
Michael Lesirge <michaellesirge@gmail.com>
2022-12-10 17:08:42 -0800

    add spliter funtion

--- better-tic-tac-toe.py
+++ better-tic-tac-toe.py
@@ -1,19 +1,25 @@
-def create_board(*dimentions):
+def create_board(*dimentions: int) -> list:
     if len(dimentions) == 0:
         return None
     return [
-       create_board(*dimentions[:-1]) for i in range(dimentions[-1])
+        create_board(*dimentions[:-1]) for i in range(dimentions[-1])
     ]
 
+
+def split_ints(s: str, seps=(",", "x")):
+    final_sep = seps[-1]
+    for sep in seps[:-1]:
+        s = s.replace(sep, final_sep)
+
+    return [int(val.strip()) for val in s.split(final_sep) if val.strip() != ""]
+
 class Board:
-    def __init__(self, *dimentions) -> None:
+    def __init__(self, *dimentions: int) -> None:
         self.board = create_board(*dimentions)
         self.dimentions = dimentions
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}{str(self.dimentions)}"
-    
+        return f"<'{__name__}.{self.__class__.__name__}' board={self.board}, dimentions={str(self.dimentions)}>"
 
 
-x = Board(4)
-print(x)
+print(split_ints("2,3,,,4"))
