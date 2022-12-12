440d9e11b2d17c5a369cdfa481b93731a8a3fb8c
Michael Lesirge <michaellesirge@gmail.com>
2022-12-11 22:05:00 -0800

    create cell class

--- better-tic-tac-toe.py
+++ better-tic-tac-toe.py
@@ -1,14 +1,15 @@
 from math import ceil
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-ALPHABET_INDEXS = {item: index for index, item in enumerate(ALPHABET)}
 
 
-def create_n_dimentional_list(dimentions: list[int]) -> list:
+def create_n_dimentional_list(dimentions: list[int], fill_factory, *, indexs = None) -> list:
+    if indexs is None:
+        indexs = []
     if len(dimentions) == 0:
-        return None
+        return fill_factory(indexs)
     return [
-        create_n_dimentional_list(dimentions[:-1]) for i in range(dimentions[-1])
+        create_n_dimentional_list(dimentions[1:], fill_factory, indexs = indexs + [i]) for i in range(dimentions[0])
     ]
 
 
@@ -27,13 +28,53 @@ def centered_padding(val: str, amount: int, *, padding: str = " "):
 
     return (padding * (side_amount + extra)) + str(val) + (padding * side_amount)
 
+class Cell:
+    to_int_chars = 2
+
+    def __init__(self, indexs: list[int], board_dimentions: list[int], *, human_readable_offset=1) -> None:
+        self.indexs = indexs
+        self.board_dimentions = board_dimentions
+
+        self.human_readable_offset = human_readable_offset
+        self.human_readable_loc = self._create_human_readable_loc()
+
+        self.val = None
+
+    # def __repr__(self) -> str:
+        # return f"<'{__name__}.{self.__class__.__name__}' val={self.val} loc={self.loc}, human_readable_loc={self.human_readable_loc}>"
+
+    def _create_human_readable_loc(self) -> str:
+        if len(self.indexs) == 0:
+            return ""
+
+
+        to_int_amount = min(self.to_int_chars, len(self.indexs))
+
+        letters = "".join([ALPHABET[index] for index in self.indexs[:-to_int_amount]])
+
+        num = 0
+        for i in range(to_int_amount, 0, -1):
+            lower = i-1
+            multiplyer = 1 if lower == 0 else self.board_dimentions[-lower]
+            num += self.indexs[-i] * multiplyer
+
+        return letters + str(num + self.human_readable_offset)
+
+    def __repr__(self) -> str:
+        return f"<'{__name__}.{self.__class__.__name__}' val={self.val}, indexs={self.indexs}, human_readable_loc={self.human_readable_loc}>" 
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return self.human_readable_loc if self.val is None else str(self.val)
 
 class Board:
     def __init__(self, dimentions: list[int]) -> None:
         if any([item == 0 for item in dimentions]):
             dimentions = []
         self.dimentions = dimentions
-        self.board = create_n_dimentional_list(self.dimentions)
+        self.board = create_n_dimentional_list(self.dimentions, lambda indexs: Cell(indexs, self.dimentions))
 
         self.step_amount = 2
 
@@ -49,8 +90,6 @@ class Board:
         # simple - UTF-8
         # self.verticle_line_char, self.horizontal_line_char, self.cross_line_char = "|", "+", "-"
 
-        self.str_template = self._create_template()
-
     def get(self, indexes: list[int]) -> str:
         if len(indexes) != len(self.dimentions):
             raise ValueError(
@@ -65,76 +104,20 @@ class Board:
     def set(self, indexes: list[int], value: str) -> None:
         if len(indexes) != len(self.dimentions):
             raise ValueError(
-                f"Can not set value {repr(value)} location {str(indexes)[1:-1]} on {len(self.dimentions)}D board.")
+                f"Can not set value {repr(value)} location {str(indexes)[1:-1]} on {len(self.dimentions)}D board."
+            )
         val = self.board
-        indexes = list(reversed(indexes))
         for index in indexes[:-1]:
             val = val[index]
         val[indexes[-1]] = value
 
     def __repr__(self) -> str:
-        return f"<'{__name__}.{self.__class__.__name__}' board={self.board}, dimentions={str(self.dimentions)}>"
-
-    def _create_template(self) -> str:
-        # luckly this only happens once so it does not need to be super efficent
-        def create_template(v, make_horizontal) -> str | list:
-            """
-            verticle, horizontal, verticle, horizontal...
-            """
-            if not isinstance(v, list):
-                return "%s"
-
-            v = [create_template(item, not make_horizontal) for item in v]
-
-            if make_horizontal:
-                rows = [item.split("\n") for item in v]
-                sep = " " + self.verticle_line_char + " "
-
-                if len(rows[0]) == 1:
-                    return " " + (sep).join(v) + " "
-
-                final = ""
-
-                for i in range(len(rows) // 2):
-                    for j in range(len(rows[i])):
-                        final += rows[i][j] + sep + rows[i+1][j] + "\n"
-
-                return final
-
-            else:
-                cells_count = v[0].count("%s")
-                sep = self.horizontal_line_char * (self.cell_size + 2)
-
-                return ("\n" + (self.cross_line_char.join([sep] * cells_count)) + "\n").join(v)
-
-        return create_template(self.board, len(self.dimentions) % 2 == 1)
+        return f"<'{__name__}.{self.__class__.__name__}' board={self.board}, dimentions={self.dimentions}>"
 
     def __str__(self) -> str:
-        vals = []
+        return repr(self)
 
-        def make_vals(v: list | str, add=""):
-            if not isinstance(v, list):
-                if v is None:
-                    if len(add) == 0:
-                        v = ""
-                    elif len(add) == 1:
-                        v = str(ALPHABET_INDEXS[add] + self.offset)
-                    else:
-                        num = ((ALPHABET_INDEXS[add[-2]])*self.dimentions[0]) + ALPHABET_INDEXS[add[-1]]
-
-                        v = str(num + self.offset)
-                        if len(add)-2 > 0:
-                            v = add[:-2] + v
-
-                vals.append(centered_padding(str(v), self.cell_size))
-            else:
-                for index, item in enumerate(v):
-                    make_vals(item, add + (ALPHABET[index]))
-
-        make_vals(self.board)
-
-        return self.str_template % tuple(vals)
-
-
-board = Board([3, 3, 2])
+board = Board([2, 2, 2, 2])
+print(board.board)
+print()
 print(board)
