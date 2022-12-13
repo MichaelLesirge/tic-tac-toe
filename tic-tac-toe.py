ed85901bca3e6b3190b30fb64d8ea3e312051562
Michael Lesirge <michaellesirge@gmail.com>
2022-12-12 22:41:36 -0800

    make new board use array

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,10 +4,11 @@
 def main():
     print("Welcome to tic-tac-toe with python!")
 
-    color_mode = bool_input("\u001b[31m Is this red for you \033[0m")
-    # color_mode = bool_input("Does you console support ASCII color codes")
-    Player.color_mode = color_mode
-    print()
+    # color_mode = bool_input("Does you console support ASCII color codes if your not sure, \u001b[31mis this red for you\033[0m")
+    # Player.color_mode = color_mode
+    # print()
+
+    Player.color_mode = True
 
     if bool_input("Do you want a custom board"):
         message = "Enter the %s of the board: "
@@ -19,7 +20,12 @@ def main():
 
     print()
 
-    players = []
+    if bool_input("Do you want a custom win condtion"):
+        pass
+
+    print()
+
+    players: list[Player] = []
     if bool_input("Do you want a custom players"):
         for i in range(1, int_input("Enter the number of players: ") + 1):
             print()
@@ -32,30 +38,40 @@ def main():
 
     print()
 
+    ties_count = 0
+    game_count = 0
+
     playing = True
     while playing:
-        board.new_game()
-        print(f"Round {board.rounds_played}")
-        while board.is_not_full():
+        board.reset()
+        print(f"Round {game_count+1}")
+
+        in_game = True
+        while in_game:
             player = players[board.placed % len(players)]
             print(board)
             print(f"{player}'s turn.")
 
-            player.place()
+            player.take_turn(on=board)
 
             if board.is_winner(player):
                 print(board)
                 player.wins += 1
                 print(f"Player {player} wins!")
-                break
-        else:
-            print(board)
-            print("It's a tie!")
+                in_game = False
+            elif board.is_full():
+                print(board)
+                ties_count += 1
+                print("It's a tie!")
+                in_game = False
         print()
 
+        print(f"Ties: {ties_count}")
         for player in players:
             print(f"{player}: {player.wins}")
         print()
+
+        game_count += 1
 
         playing = bool_input("Do you want to play again")
 
@@ -63,84 +79,60 @@ def main():
 
 
 class Board:
-    def __init__(self, width=3, height=3):
+    DEFAULT_SIZE = 3
+    OFFSET = 1
+
+    def __init__(self, width=DEFAULT_SIZE, height=DEFAULT_SIZE):
         self.width: int = width
         self.height: int = height
         self.size: int = (self.width * self.height)
 
-        self.rounds_played: int = 0
         self.placed: int
-        self._board: dict
-        self._reset()
+        self.board: list[list]
+        self.reset()
 
-    def new_game(self):
-        self.rounds_played += 1
-        self._reset()
-
-    def _reset(self):
+    def reset(self):
         self.placed = 0
-        self._board = {i: str(i) for i in range(1, self.size + 1)}
+        self.board = [[None for col in range(self.width)]
+                      for row in range(self.height)]
 
-    def _is_valid_location(self, loc) -> bool:
-        return loc in self._board
+    def is_valid_location(self, row, col) -> bool:
+        return (row < self.height) and (col < self.width)
 
-    def _is_empty_location(self, loc) -> bool:
-        return self._board[loc] == str(loc)
+    def is_empty_location(self, row, col) -> bool:
+        return self.board[row][col] is None
 
     def is_full(self) -> bool:
         return self.placed >= self.size
 
-    def is_not_full(self) -> bool:
-        return self.placed < self.size
-
     def is_winner(self, player) -> bool:
-        for i in range(1, self.size + 1, self.width):
-            if all(((self._board[j] == player) for j in range(i, i + self.width))):
-                return True
-
-        for i in range(1, self.width + 1):
-            if all(((self._board[j] == player) for j in range(i, self.size + 1, self.width))):
-                return True
-
-        if self.width == self.height:
-            if all(((self._board[i] == player) for i in range(1, self.size + 1, self.width + 1))) or \
-                    all(((self._board[i] == player) for i in range(self.width, self.size, self.width - 1))):
-                return True
+        # todo
         return False
 
-    def __getitem__(self, item):
-        return self._board[item]
-
-    def __setitem__(self, key, value):
-        self.place(key, value)
-
     def place(self, loc: int, val: "Player"):
-        if not self._is_valid_location(loc):
+        loc -= self.OFFSET
+
+        row = loc // self.width
+        col = loc - (self.width * row)
+
+        if not self.is_valid_location(row, col):
             raise ValueError(f"location must be from 1 to {self.size}")
-        if not self._is_empty_location(loc):
+        if not self.is_empty_location(row, col):
             raise ValueError("location is already occupied")
+
+        self.board[row][col] = val
+
         self.placed += 1
-        self._board[loc] = val
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self._board})"
+        return f"{self.__class__.__name__}({self.board})"
 
     def __str__(self):
-        max_num_len = len(str(self.size))
-        rows = []
-        row = []
-        for i in range(1, self.size + 1):
-            row.append(add_buffer(self._board[i], max_num_len))
-            if i % self.width == 0:
-                rows.append(" " + (" | ".join(row)) + " \n")
-                row = []
-
-        line = "-" + ("-" * max_num_len) + "-"
-        return "\n" + (((line + "+") * (self.width - 1)) + line + "\n").join(rows)
+        # I'm so sorry future self, but I realised it was possible and this projected does not matter so I just did it.
+        return "\n" + (("\n" + "┼".join(["-" + ("-" * len(str(self.size))) + "-"] * self.width) + "-" + "\n").join([" " + ((" " + "│" + " ").join([centered_padding(str(item if item is not None else ((i*self.width)+j+self.OFFSET)), len(str(self.size))) for j, item in enumerate(row)]) + " ") for i, row in enumerate(self.board)])) + "\n"
 
 
 class Player:
-    board = None
     color_mode = False
 
     colors = {
@@ -160,21 +152,25 @@ class Player:
             raise ValueError("Player character must be a letter")
         self.char = char.upper()
 
-        color = color.lower()
-        if color not in Player.colors:
-            raise ValueError(f"{color} is not an available color")
-        self.color: str = Player.colors[color]
+        if (color is not None) and Player.color_mode:
+            color = color.lower()
+            if color not in Player.colors:
+                possible_colors = list(self.colors.keys())
+                raise ValueError(
+                    f"\"{color}\" is not an available color. Try {', '.join(possible_colors[:-1])} or {possible_colors[-1]}")
+            color = Player.colors[color]
+        self.color = color
 
         self.wins = 0
 
-    def place(self):
+    def take_turn(self, on: Board):
         while True:
             try:
                 print()
                 loc = input("Enter where you want to go: ")
                 if not loc.isnumeric():
                     raise ValueError("Location must be a number")
-                Player.board.place(int(loc), self)
+                on.place(int(loc), self)
                 return
             except ValueError as exs:
                 print_invalid(exs)
@@ -188,7 +184,7 @@ class Player:
         return self.char
 
 
-def add_buffer(val, amount, *, buffer=" "):
+def centered_padding(val, amount, *, buffer=" "):
     amount -= (1 if isinstance(val, Player) else len(val))
 
     side_amount, extra = divmod(amount, 2)
@@ -219,14 +215,19 @@ def create_player():
     while True:
         try:
             char = input("Letter: ").strip()
-            player = player(char)
-            if Player.color_mode:
-                color = input("Color: ").strip()
-            player = Player(char, color)
+            player = Player(char)
+            break
         except ValueError as exs:
             print_invalid(exs)
-        else:
-            return player
+    if Player.color_mode:
+        while True:
+            try:
+                color = input("Color: ").strip().lower()
+                player = Player(char, color)
+                break
+            except ValueError as exs:
+                print_invalid(exs)
+    return player
 
 
 if __name__ == "__main__":
