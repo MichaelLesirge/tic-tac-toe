46b32dab52f3d603ddf3a096d2fe97495668929d
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-01-21 22:59:10 -0800

    refactor index.js

--- HTML-CSS-JS/scripts/index.js
+++ HTML-CSS-JS/scripts/index.js
@@ -1,62 +1,52 @@
 import { MIN_SIZE, SUGGESTED_MAX_SIZE, DEFAULT_SIZE } from "./consts.js";
 
-const boardSizeInputs = document.querySelectorAll("input.board-size");
-const maxInputLen = SUGGESTED_MAX_SIZE.toString().length;
-const sizeInputs = document.querySelectorAll("input.size");
-const winConstionInput = document.querySelector("input#win-condition");
 const form = document.getElementById("create-game-form");
 
-let max = -1;
-function updateMax() {
-	max = -1;
-	boardSizeInputs.forEach((el) => {
+const sizeInputs = form.querySelectorAll("input.size");
+const boardSizeInputs = form.querySelectorAll("input.size.board-size");
+const winConditionInput = form.querySelector("input.size#win-condition");
+
+const winConstionExplanation = form.querySelector("#explanation");
+
+// only allow valid input
+sizeInputs.forEach((input) => {
+	input.setAttribute("min", MIN_SIZE);
+	input.setAttribute("max", SUGGESTED_MAX_SIZE);
+
+	input.addEventListener("keypress", (event) => {
+		// if key is not a number of the number is to long ignore key press
+		if (!/\d/.test(event.key) || event.target.value.length >= event.target.getAttribute("max").toString().length) {
+			event.preventDefault();
+			return false;
+		}
+	});
+	
+	input.addEventListener("input", updateWinCondtionSettings);
+});
+
+boardSizeInputs.forEach((input) => {
+	input.setAttribute("value", DEFAULT_SIZE);
+});
+
+updateWinCondtionSettings()
+
+function updateWinCondtionSettings() {
+	const maxPossibleWinCondition = getMaxInput(boardSizeInputs);
+	winConditionInput.setAttribute("max", maxPossibleWinCondition);
+
+	if (maxPossibleWinCondition < winConditionInput.value) {
+		winConstionExplanation.innerText = `Impossible to get ${winConditionInput.value} in a row with current board sizes.`;
+	} else if (winConditionInput.value) {
+		winConstionExplanation.innerText = `Get ${winConditionInput.value} in a row to win.`;
+	} else {
+		winConstionExplanation.innerText = `Get all the way across to win.`;
+	}
+};
+
+function getMaxInput(inputElements) {
+	let max = -Infinity;
+	inputElements.forEach((el) => {
 		if (el.value > max) max = el.value;
 	});
-	winConstionInput.max = max;
-	displayCaption();
+	return max;
 }
-
-boardSizeInputs.forEach((el) => {
-	el.setAttribute("value", DEFAULT_SIZE); // must use setAttribute for value otherwise built in reset to default does not function
-	el.oninput = () => {
-		validInput(el);
-		updateMax();
-	};
-});
-
-function validInput(input) {
-	input.value = input.value.replace("-", "");
-	if (input.value.length > maxInputLen) input.value = input.value.slice(0, maxInputLen);
-}
-
-sizeInputs.forEach((el) => {
-	el.min = MIN_SIZE;
-	el.max = SUGGESTED_MAX_SIZE;
-});
-
-
-const winConstionExplanation = document.getElementById("explanation");
-function displayCaption() {
-	if (max < winConstionInput.value) {
-		winConstionExplanation.innerText = `Impossible to get ${winConstionInput.value} in a row with current board sizes.`;
-	} else {
-		winConstionExplanation.innerText =
-			"Get " +
-			(winConstionInput.value == ""
-				? "all the way across"
-				: `${winConstionInput.value} in a row`) +
-			" to win.";
-	}
-}
-
-winConstionInput.oninput = () => {
-	validInput(winConstionInput);
-	displayCaption();
-};
-
-updateMax();
-displayCaption();
-form.onreset = () => {
-	updateMax();
-	displayCaption();
-};
